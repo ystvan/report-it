9b7f715e50f22b03cabf97a3cc9467705e57f3b3
Istvan Marki <istv0050@edu.easj.dk>
2017-04-01 21:28:33 +0200

    getttin' smarter

--- dialog.js
+++ dialog.js
@@ -18,14 +18,17 @@ const options = [
 
 module.exports = dialog
     .matches('getting the episode title', [
-        confirmTitle, getTitle
+        confirmTitle, 
+        getTitle
     ])
-    // .matches('getting the episode plot', [
-    //     confirmPlot, getPlot
-    // ])
-    // .matches('getting the episode release_date', [
-
-    // ])
+    .matches('getting the episode plot', [
+        confirmPlot, 
+        getPlot
+    ])
+    .matches('getting the episode release_date', [
+        confirmRelease, 
+        getRelease
+    ])
     //  .matches('getting the cast', [
         
     // ])
@@ -37,13 +40,25 @@ module.exports = dialog
     // ])
     .onDefault([sendInstructions]);
 
+
+//==============================================
 //default Prompt in case of non applicable query
+//==============================================
+
+
 function sendInstructions(session, results, next) {
-    builder.Prompts.choice(session, 'Ask me about STAR WARS movies!', options);
+    builder.Prompts.choice(session, 'Better ask me about STAR WARS movies!', options);
     next();
 }
 
-// After luis recognized that user probably looking for the episode's name, confirm:
+
+//======================================================================================
+// After luis recognized that user probably looking for the episode's name, confirm back:
+//======================================================================================
+
+
+
+
 function confirmTitle(session, args, next) {
     session.dialogData.entities = args.entities;
 
@@ -55,7 +70,32 @@ function confirmTitle(session, args, next) {
     }
 }
 
+function confirmPlot(session, args, next) {
+    session.dialogData.entities = args.entities;
+
+    var films = builder.EntityRecognizer.findEntity(args.entities, 'films');
+    if (films) {
+        next({ response: films.entity });
+    } else {
+        builder.Prompts.text(session, 'Need some poilers? Which episode are you interested in?');
+    }
+}
+
+function confirmRelease(session, args, next) {
+    session.dialogData.entities = args.entities;
+
+    var films = builder.EntityRecognizer.findEntity(args.entities, 'films');
+    if (films) {
+        next({ response: films.entity });
+    } else {
+        builder.Prompts.text(session, 'Some history huh? Which episode release date you want?');
+    }
+}
+
+//=============================================================
 // After confirmation getting the Episode name by using the API
+//=============================================================
+
 
 function getTitle(session, results, next) {
     var films = results.response;
@@ -66,22 +106,72 @@ function getTitle(session, results, next) {
     if (!films) {
         session.endDialog('Request cancelled.');
     } else {
-        loadTitle(films, (title) => {
-            if (title !== 'Not Found') {
-                var message = new builder.Message(session).text('The title of the movie is ' + title);
+        loadTitle(films, (dobject) => {
+            if (dobject == undefined) {
+                var message = new builder.Message(session).text('The title of the movie is ' + dobject);
                 session.send(message);
             } else {
-                session.endDialog('Sorrrrrrrrrrrrrrrrrry');
+                session.endDialog('Sorry buddy, I am too tired');
             }
         });
     }
 }
 
 
+function getPlot(session, results, next) {
+    var films = results.response;
 
+    if (films.entity) films = session.dialogData.films = films.entity;
+    else session.dialogData.user = films;
+
+    if (!films) {
+        session.endDialog('Request cancelled.');
+    } else {
+        loadPlot(films, (dobject) => { 
+            if (dobject == undefined) {
+                var message = new builder.Message(session).text('Long story short: ' + dobject);
+                session.send(message);
+            } else {
+                session.endDialog('Sorry, I could not quite grasp what you said... :(');
+            }
+        });
+    }
+}
+
+function getRelease(session, results, next) {
+    var films = results.response;
+
+    if (films.entity) films = session.dialogData.films = films.entity;
+    else session.dialogData.user = films;
+
+    if (!films) {
+        session.endDialog('Request cancelled.');
+    } else {
+        loadDate(films, (dobject) => {
+            if (dobject == undefined) {
+                var message = new builder.Message(session).text('The episode has been released in year: ' + dobject);
+                session.send(message);
+            } else {
+                session.endDialog('I think I am too rookie for that');
+            }
+        });
+    }
+}
+
+//=============================================================
 //Helper methods from the API requests
+//=============================================================
+
 
 function loadTitle(films, callback){
+    loadData('/api/films/' + querystring.escape(films), callback);
+}
+
+function loadPlot(films, callback){
+    loadData('/api/films/' + querystring.escape(films), callback);
+}
+
+function loadDate(films, callback){
     loadData('/api/films/' + querystring.escape(films), callback);
 }
 
@@ -94,7 +184,7 @@ function loadData(path, callback) {
             'User-Agent': 'sample-bot'
         }
     };
-    var title;
+    var dobject;
     var request = https.request(options, function (response) {
         var data = '';
         response.on('data', function (chunk) { data += chunk; });
@@ -105,23 +195,5 @@ function loadData(path, callback) {
     });
     request.end();
 }  
-    
-//creating cards for fancy responses
-/*
-function getFilmThumbnail(session, profile) {
-    var thumbnail = new builder.ThumbnailCard(session);
-    thumbnail.title(films.title);
-    thumbnail.images([builder.CardImage.create(session, '/src/images/swlogo.png')]);
-
-    if(films.title) thumbnail.subtitle(films.title);
-
-    var text = '';
-    if (films.title) text += films.title + ' \n';
-    
-    thumbnail.text(text);
-   
-    return thumbnail;
-}
-*/
 
 
